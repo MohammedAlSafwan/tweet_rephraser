28fee077f8994d4db951d5c03bcdc72e29c9beb1
Mohammed Al-Safwan <alsafwan.mohammed+github@gmail.com>
2023-04-27 23:58:06 -0400

    clean up

--- config.py
+++ config.py
@@ -4,5 +4,5 @@ TWITTER_ACCESS_TOKEN = "your_twitter_access_token"
 TWITTER_ACCESS_SECRET = "your_twitter_access_secret"
 OPENAI_API_KEY = "your_openai_api_key"
 
-LIST_ID="your_list_to_get_tweets"
-TARGET_ACCOUNT="your_account_to_post_tweets"
+LIST_ID="your_twitter_list_id"
+TARGET_ACCOUNT="your_target_account_username"
